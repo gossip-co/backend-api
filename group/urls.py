c1341bab73bed40bf38b9c60c08dede4793de936
aditya-pushkar <adityapushkar850@gmail.com>
2023-02-28 19:39:29 +0530

    group list added

--- group/urls.py
+++ group/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     create_group, edit_group_detail,
-    search_group, is_group_created_by_user
+    search_group, is_group_created_by_user,
+    groups
 )
 
 urlpatterns = [
     path('create/', create_group, name="create-group"),
+    path('list/', groups, name="groups"),
     path('edit/', edit_group_detail, name="edit-group-detail"),
     path('search/<str:group_slug>/', search_group, name="search-group"),
     path('is_created/', is_group_created_by_user, name="is_group_created_by_user")
--- group/views.py
+++ group/views.py
@@ -7,9 +7,18 @@ from rest_framework.status import (
     HTTP_201_CREATED,
 )
 
-
 from .models import Group
 from .serializers import GroupSerializer
+
+@api_view(['GET'])
+def groups(request):
+    try:
+        group_obj = Group.objects.all()
+        serializer = GroupSerializer(group_obj, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+    except Exception as error:
+        return Response(status=HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def create_group(request):
@@ -43,14 +52,17 @@ def edit_group_detail(request):
 
 
 # For M.V.P we don't need query because group_unique_code is slug_filed which is unique so we can, simple get this by .get method 
+# is_lauched should be True , or return 404
 @api_view(['GET'])
 def search_group(request, group_slug):
     try:
         group_object = Group.objects.get(slug=group_slug)
+        if not group_object.is_launched:
+            return Response(status=HTTP_404_NOT_FOUND)
         serializer = GroupSerializer(group_object, many=False)
         return Response(serializer.data, status=HTTP_200_OK)
     except ObjectDoesNotExist:
-        return Response(HTTP_404_NOT_FOUND)
+        return Response(status=HTTP_404_NOT_FOUND)
 
 # current user, 
 @api_view(['GET'])
